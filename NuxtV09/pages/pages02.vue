4437ce33690eb12c53392e943bf4c80ac64ba7d3
songrit0 <songrit208133@gmail.com>
2022-11-04 16:09:59 +0700

    add axios

--- NuxtV09/pages/pages02.vue
+++ NuxtV09/pages/pages02.vue
@@ -3,12 +3,33 @@
 </template>
 
 <script>
-
+import { Axios, axios } from 'axios'
+import { ref } from 'vue'
 export default ({
     data() {
         return {
             demo: 'Hello',
+            news:[],
         }
     },
+    mounted() {
+        const url = `https://newsapi.org/v2/everything?q=tesla&from=2022-10-04&sortBy=publishedAt&apiKey=e50d40f57ef54b4fab3add95a765efa1`
+        axios.get(url).then((response) =>{
+            console.log(response);
+            then.news = response.data
+        })
+
+        // const st = [1,2,]
+         
+        //     const data = async  () => {
+        //     const response = await axios.get(`https://newsapi.org/v2/everything?q=tesla&from=2022-10-04&sortBy=publishedAt&apiKey=e50d40f57ef54b4fab3add95a765efa1`)
+        //     st.value = response.data.articles
+        // }
+        // console.log(st);
+        // console.log(data);
+        // console.log('aa');
+        
+        
+    }, 
 })
 </script>
